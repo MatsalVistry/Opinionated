4494d3c21ec8e797353ab5b4a9068f1f037fa756
Vatsal Mistry <vatsalmistry4796@gmail.com>
2019-03-04 20:45:14 -0600

    update and delete debate

--- politics/urls.py
+++ politics/urls.py
@@ -9,10 +9,7 @@ urlpatterns = [
     
     #/politics/debate id/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    
-    
-    
     url(r'debate/add/$', views.DebateCreate.as_view(), name = 'debate-add'),
-
-
+    url(r'debate/(?P<pk>[0-9]+)/$', views.DebateUpdate.as_view(), name = 'debate-update'),
+    url(r'debate/(?P<pk>[0-9]+)/delete/$', views.DebateDelete.as_view(), name = 'debate-delete'),
 ]
--- politics/views.py
+++ politics/views.py
@@ -1,6 +1,12 @@
 from django.views import generic
 from .models import Debate
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render
+from urllib import request
+from django.http import HttpResponse
+from django.template import loader
+from django.template import Context
+from django.urls.base import reverse_lazy
 
 class IndexView(generic.ListView):
     template_name = 'politics/index.html'
@@ -16,3 +22,11 @@ class DebateCreate(CreateView):
     model = Debate
     fields = ['username','title', 'description']
     
+class DebateUpdate(UpdateView):
+    model = Debate
+    fields = ['username','title', 'description']
+
+class DebateDelete(DeleteView):
+    model = Debate
+    success_url = reverse_lazy('politics:index')
+
